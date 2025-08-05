d84feebd3cea0bf49035da4ebdf55ac2e27c8c23
Ekan <ekanessien@gmail.com>
2025-08-04 18:16:08 -0700

    Advances in Background Images Slideshow

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-"use strict"
+"use strict";
+
+//Slideshow
+const slideshowDivs = () => {
+  for (let i = 1; i <= 5; i++) {
+    const div = document.createElement("div");
+
+    div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
+
+    i === 1 && div.classList.add("change");
+
+    document.querySelector(".slideshow").appendChild(div);
+  }
+};
+
+slideshowDivs();
+//End of Slideshow
